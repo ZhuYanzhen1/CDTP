8ffeffb4d793877f78cd11545dd79732aa208696
LaoZhu <2208213223@qq.com>
2020-03-04 21:12:55 +0800

    Repair 0xff bug

--- Mid Capacity/src/decrypt.c
+++ Mid Capacity/src/decrypt.c
@@ -15,33 +15,46 @@ void Comm_Received_CallBack(unsigned char Receive_Buffer[])
 	}
 	Buffer_Data_Counter++;
 	if(Buffer_Data_Counter == 12)   //if the package had been broken
-  {
-    unsigned char Data_Buffer[8];
-    unsigned char PID_Bit = Receive_Buffer[1]>>4;  //Get The PID Bit
-    if(PID_Bit == ((~(Receive_Buffer[1] & 0x0f)) & 0x0f) )  //PID Verify Success
-    {
-      unsigned char Temp_Var = 0x00 ;
-      while(1)     //Memory Copy
-      {
-        Data_Buffer[Temp_Var] = Receive_Buffer[2+Temp_Var];
-        Temp_Var++;
-        if(Temp_Var == 8)
-					break;
-      }
-      if(Receive_Buffer[10] != 0x00)   //Some Byte had been replace
-      {
-        unsigned char Temp_Filter = 0x00;
-				Temp_Var = 0;
-        while(1)
         {
-          if(((Receive_Buffer[10] & (Temp_Filter|(0x01<<Temp_Var)))>>Temp_Var) == 1)  //This Byte Need To Adjust
-             Data_Buffer[Temp_Var] = 0xff;    //Adjust to 0xff
-          Temp_Var++;
-          if(Temp_Var == 8)
-            break;
+            unsigned char Data_Buffer[8];
+            unsigned char PID_Bit = Receive_Buffer[1]>>4;  //Get The PID Bit
+            if(PID_Bit == ((~(Receive_Buffer[1] & 0x0f)) & 0x0f) )  //PID Verify Success
+            {
+              unsigned char tmp_buf[12];
+              memset(tmp_buf,0xa5,sizeof(tmp_buf));
+              tmp_buf[0] = 0xff;
+              tmp_buf[1] = Receive_Buffer[1];
+              tmp_buf[11] = 0xff;
+              if(strcmp(Receive_Buffer,tmp_buf) == 0)
+              {
+                    memset(Data_Buffer,0xff,sizeof(Data_Buffer));
+                    Receive_CallBack(PID_Bit, Data_Buffer);
+              }
+              else
+              {
+                  unsigned char Temp_Var = 0x00 ;
+                  while(1)     //Memory Copy
+                  {
+                    Data_Buffer[Temp_Var] = Receive_Buffer[2+Temp_Var];
+                    Temp_Var++;
+                    if(Temp_Var == 8)
+                                                    break;
+                  }
+                  if(Receive_Buffer[10] != 0x00)   //Some Byte had been replace
+                  {
+                    unsigned char Temp_Filter = 0x00;
+                    Temp_Var = 0;
+                    while(1)
+                    {
+                      if(((Receive_Buffer[10] & (Temp_Filter|(0x01<<Temp_Var)))>>Temp_Var) == 1)  //This Byte Need To Adjust
+                         Data_Buffer[Temp_Var] = 0xff;    //Adjust to 0xff
+                      Temp_Var++;
+                      if(Temp_Var == 8)
+                        break;
+                    }
+                  }
+                  Receive_CallBack(PID_Bit, Data_Buffer);
+                }
+            }
         }
-			}
-      Receive_CallBack(PID_Bit, Data_Buffer);
-    }
-	}
 }
--- Mid Capacity/src/encrypt.c
+++ Mid Capacity/src/encrypt.c
@@ -1,23 +1,38 @@
 #include "encrypt.h"
 void Send_Data(unsigned char pid, unsigned char Data[8])
 {
-	unsigned char temp_buf[12];
-	unsigned char cnt = 0;
-	memset(temp_buf,0x00,sizeof(temp_buf));
-	temp_buf[0] =  0xff;										//set start of frame
-	temp_buf[10] = 0x00;										//initialize adjust frame
-	for(cnt = 0;cnt < 8;cnt++)							//copy data and adjust
-	{
-                if(Data[cnt] == 0xff)									//if data equal 0xff, mark it
-		{
-			temp_buf[2 + cnt] = 0x00;
-			temp_buf[10] = (temp_buf[10] | (1 << cnt));
-		}
-		else
-                        temp_buf[2 + cnt] = Data[cnt];
-	}
-	temp_buf[1] = pid<<4 | ((~pid) & 0x0f);	//set pid and check
-	temp_buf[11] = 0xff;										//set end of frame
-	for(cnt = 0;cnt < 12;cnt++)
-		Comm_SendChar(temp_buf[cnt]);
+    unsigned char tmp_buf[8];
+    unsigned char temp_buf[12];
+    unsigned char cnt = 0;
+    memset(tmp_buf,0xff,sizeof(tmp_buf));
+    if(strcmp(Data,tmp_buf) == 0)
+    {
+        memset(temp_buf,0xa5,sizeof(temp_buf));
+        temp_buf[0] =  0xff;										//set start of frame
+        temp_buf[10] = 0x00;
+        temp_buf[1] = pid<<4 | ((~pid) & 0x0f);	//set pid and check
+        temp_buf[11] = 0xff;
+        for(cnt = 0;cnt < 12;cnt++)
+            Comm_SendChar(temp_buf[cnt]);
+    }
+    else
+    {
+        memset(temp_buf,0x00,sizeof(temp_buf));
+        temp_buf[0] =  0xff;										//set start of frame
+        temp_buf[10] = 0x00;										//initialize adjust frame
+        for(cnt = 0;cnt < 8;cnt++)							//copy data and adjust
+        {
+            if(Data[cnt] == 0xff)									//if data equal 0xff, mark it
+            {
+                temp_buf[2 + cnt] = 0x00;
+                temp_buf[10] = (temp_buf[10] | (1 << cnt));
+            }
+            else
+                temp_buf[2 + cnt] = Data[cnt];
+        }
+        temp_buf[1] = pid<<4 | ((~pid) & 0x0f);	//set pid and check
+        temp_buf[11] = 0xff;										//set end of frame
+        for(cnt = 0;cnt < 12;cnt++)
+            Comm_SendChar(temp_buf[cnt]);
+    }
 }
